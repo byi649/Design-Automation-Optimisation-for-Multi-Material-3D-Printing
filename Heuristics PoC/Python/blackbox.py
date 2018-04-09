53f03c588871d7da2024d67c4148724c4fbc9e26
byi649 <byi649@aucklanduni.ac.nz>
2018-04-10 00:07:43 +1200

    Fix NaN error

--- Heuristics PoC/Python/blackbox.py
+++ Heuristics PoC/Python/blackbox.py
@@ -8,16 +8,14 @@ def blackbox(E, rho):
     w = 2e-3
     h = 2e-2
     L = 1e-1
-    I = 1/12 * h * w**3
+    I = 1./12. * h * w**3
     A = w * h
-
-    E = E[0]
 
     def alphaFormula(n):
         if n in range(4):
             alpha = [1.875, 4.694, 7.855, 10.996]
             return alpha[n - 1]
-        return (2 * n - 1) * math.pi / 2
+        return (2. * n - 1.) * math.pi / 2.
 
     alpha = []
     for i in range(10):
@@ -25,7 +23,11 @@ def blackbox(E, rho):
 
     freq = []
     for i in range(10):
-        freq.append(alpha[i]**2 / (2 * math.pi * L**2) * math.sqrt((E * I) / (rho * A)))
+        # Sometimes returns NaN ???
+        freq.append(alpha[i]**2 / (2. * math.pi * L**2) * math.sqrt((E * I) / (rho * A)))
+        # print(rho, A, rho*A)
+        # print(alpha[i]**2 / (2. * math.pi * L**2) * math.sqrt((E * I) / (rho * A)))
+        # print(E, I, rho, A)
 
     return freq
 
@@ -43,17 +45,18 @@ def fitness(freq, goal):
     return fitness
 
 def fitness2(E):
-    goal = [53, 332, 930, 1822, 3013, 4501, 6287, 8370, 10751, 13429]
-    freq = blackbox(E, 1.3e3)
+    goal = [53.005922156059206, 332.20641980613505, 930.2811671037335, 1822.8783393540868, 3013.3294997485928, 4501.393450241724, 6287.070190833485, 8370.359721523866, 10751.262042312881, 13429.777153200515]
+    freq = blackbox(E[0], E[1]*1e-6)
     fitness = 0
     for i in range(10):
         fitness += (freq[i] - goal[i])**2
 
-    fitness = math.sqrt(fitness/10)
+    #fitness = math.sqrt(fitness/10.)
 
     return (fitness, )
 
 # Testing
+# print(blackbox(3.5e9, 1.3e3))
 # print(fitness(blackbox(3.5e9, 1.3e3), 1))
 # print(fitness2(3.5e9))
     
--- Heuristics PoC/Python/heuristic.py
+++ Heuristics PoC/Python/heuristic.py
@@ -1,5 +1,5 @@
 import blackbox
-import numpy
+import numpy as np
 
 from deap import base
 from deap import creator
@@ -8,7 +8,7 @@ from deap import benchmarks
 from deap import cma
 from deap import algorithms
 
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("FitnessMin", base.Fitness, weights=(-1.0, ))
 creator.create("Individual", list, fitness=creator.FitnessMin)
 
 toolbox = base.Toolbox()
@@ -16,26 +16,35 @@ toolbox = base.Toolbox()
 toolbox.register("evaluate", blackbox.fitness2)
 
 def feasible(individual):
-    return individual[0] > 0
+    feasible = ((0 < individual[0] < 1e15) and (0 < individual[1] < 1e10))
+    return feasible
 
 def distance(individual):
     """A distance function to the feasibility region."""
-    return individual[0]**2
+    distance = 0
+    if individual[0] <= 0:
+        distance += individual[0]
+    if individual[1] <= 0:
+        distance += individual[1]
+    return -distance
 
-toolbox.decorate("evaluate", tools.DeltaPenalty(feasible, 1e3, distance))
+toolbox.decorate("evaluate", tools.DeltaPenalty(feasible, 1e11, distance))
 
-N = 1
+N = 2
 
-strategy = cma.Strategy(centroid=[5e9]*N, sigma=1e9, lambda_=20*N)
+strategy = cma.Strategy(centroid=[5e9]*N, sigma=1e8, lambda_=20*N)
 toolbox.register("generate", strategy.generate, creator.Individual)
 toolbox.register("update", strategy.update)
 
 hof = tools.HallOfFame(1)
 stats = tools.Statistics(lambda ind: ind.fitness.values)
-stats.register("avg", numpy.mean)
-stats.register("std", numpy.std)
-stats.register("min", numpy.min)
-stats.register("max", numpy.max)
+stats.register("avg", np.mean)
+stats.register("std", np.std)
+stats.register("min", np.min)
+stats.register("max", np.max)
 
 algorithms.eaGenerateUpdate(toolbox, ngen=250, stats=stats, halloffame=hof)
-print(hof[0])
+print("Best solution: E = {0:.3e}, rho = {1:.3e}".format(hof[0][0], hof[0][1]*1e-6))
+
+freq = blackbox.blackbox(hof[0][0], hof[0][1]*1e-6)
+print(freq)
