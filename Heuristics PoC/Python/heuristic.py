3fb35a88e5a0d14a5de3d31f2eea802c0e78f02d
byi649 <byi649@aucklanduni.ac.nz>
2018-04-10 02:05:29 +1200

    Frequency error graph

--- Heuristics PoC/Python/heuristic.py
+++ Heuristics PoC/Python/heuristic.py
@@ -90,6 +90,13 @@ freq = blackbox.blackbox(hof[0][0], hof[0][1]*1e-6)
 print("Natural frequencies:")
 print('\n'.join('{}: {} Hz'.format(*k) for k in enumerate(freq, 1)))
 
+true_freq = [53.005922156059206, 332.20641980613505, 930.2811671037335, 1822.8783393540868, 3013.3294997485928, 4501.393450241724, 6287.070190833485, 8370.359721523866, 10751.262042312881, 13429.777153200515]
+
+errors = []
+for i in range(10):
+    errors.append(abs(freq[i] - true_freq[i]) / true_freq[i] * 100)
+errors = np.array(errors)
+
 # The x-axis will be the number of evaluations
 # Truncated at evaluation #2000 due to NaN shenanigans
 x = list(range(0, strategy.lambda_ * NGEN, strategy.lambda_))
@@ -117,5 +124,13 @@ plt.title("Blue: E, orange: rho (*1e6)")
 
 ax = plt.gca()
 ax.set_xlim(left=0, right=2000)
+
+plt.subplot(2, 2, 4)
+plt.bar(x=range(1, 11), height=errors)
+print(errors)
+plt.title("Percentage error for each mode")
+
+ax = plt.gca()
+ax.set_ylim(bottom=min(errors)-np.std(errors), top=max(errors)+np.std(errors))
 
 plt.show()
