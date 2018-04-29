d69965d9ee4e11e0f41857289bbaa3f8af952cd3
Benjamin Yi <byi649@en-353156.uoa.auckland.ac.nz>
2018-04-30 11:26:50 +1200

    Add preliminary graphing

--- 40VoxelBeam/statistics.py
+++ 40VoxelBeam/statistics.py
@@ -4,6 +4,8 @@ import algos
 import pandas as pd
 from toolkit import *
 import time
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def runHeuristic():
     NGEN = 100
@@ -39,8 +41,20 @@ def runHeuristic():
     df = pd.DataFrame(data)
     df.to_csv("data.csv")
 
+def makeGraphs():
+    sns.set()
+    data = pd.read_csv("data.csv", dtype={'time':np.float64})
+    data['fbest'] = [float(x[1:-1]) for x in data['fbest']]
+
+    ax = sns.distplot(data['time'].loc[data['fbest'] <= 1.], bins=15, kde=False, norm_hist=False, label="Reached 1% error")
+    sns.distplot(data['time'].loc[data['fbest'] > 1.], bins=15, kde=False, norm_hist=False, label="Did not reach 1% error")
+    ax.set(xlabel="Time taken (seconds)", ylabel="Number of occurences", title="Graph of evolution times for 50 iterations of GA")
+    plt.legend()
+    plt.show()
+
 def main():
-    runHeuristic()
+    #runHeuristic()
+    makeGraphs()
 
 if __name__ == "__main__":
 	main()
