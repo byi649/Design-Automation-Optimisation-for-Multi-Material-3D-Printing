87e329aef5c8f4aeef06c046f7404439bebe5c5e
byi649 <byi649@aucklanduni.ac.nz>
2018-05-02 22:57:32 +1200

    Visualisation changes

--- 40VoxelBeam/algos.py
+++ 40VoxelBeam/algos.py
@@ -463,7 +463,7 @@ def GA_voxel(verbose=False, NGEN=10, nVoxels=4):
                 count += 1
 
         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-        fitnesses = map(toolbox.evaluate, invalid_ind)
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
         for ind, fit in zip(invalid_ind, fitnesses):
             ind.fitness.values = fit
             solutions[binaryToStr(ind)] = fit
