a38469c1863d0398dd9ff220cac1ee525f2f9f46
Benjamin Yi <byi649@en-353156.uoa.auckland.ac.nz>
2018-05-23 11:20:45 +1200

    More population testing

--- MultiMaterialBeam/statistics.py
+++ MultiMaterialBeam/statistics.py
@@ -11,6 +11,7 @@ def runHeuristic():
     NGEN = 100
     verbose = False
     nVoxels = 40
+    iters = 5
 
     fbestlist = []
     #firstlist = []
@@ -20,15 +21,13 @@ def runHeuristic():
     poplist = []
     f1list = []
     gradlist = []
-    f1truelist = []
-    gradtruelist = []
 
-    popArray = [40]
-    f1Array = [50, 100, 200, 300, 500, 800, 1000, 1500, 2000]
-    gradArray = [50, 100, 200, 300, 400, 600, 800]
+    popArray = [20, 25, 30, 35, 40, 45, 50, 55, 60]
+    f1Array = [200]
+    gradArray = [600]
 
 
-    for i, config in enumerate(list(itertools.product(popArray, f1Array, gradArray))):
+    for i, config in enumerate(list(itertools.product(popArray, f1Array, gradArray))*iters):
         print("Running iteration: {}, population size: {}, f1: {}, grad: {}".format(i+1, config[0], config[1], config[2]))
         benchmark = [config[1] + config[2]*x for x in range(6)]
         np.savetxt('benchmark_frequencies.txt', benchmark, fmt = '%i')
@@ -37,9 +36,6 @@ def runHeuristic():
         (bin, fbest, best) = algos.GA_voxel(verbose, NGEN, nVoxels, config[0])
         end = time.time()
         freq = blackbox.blackbox_voxel(bin)
-
-        #slope, intercept, r_value, p_value, std_err = stats.linregress(range(1, 7), freq)
-        #fbest = r_value**2
 
         sol = binaryToStr(bin)
         #first = NGEN if (np.argmax(fbest<1)==0 and fbest[0]>=1) else np.argmax(fbest<1)
@@ -55,10 +51,7 @@ def runHeuristic():
         poplist.append(config[0])
         f1list.append(config[1])
         gradlist.append(config[2])
-        # f1truelist.append(slope + intercept)
-        # gradtruelist.append(slope)
 
-    #data = {'fbest': fbestlist, 'first': firstlist,'sol': sollist, 'freq': freqlist, 'time': timelist, 'nPop': poplist, 'f1': f1list, 'grad': gradlist, 'f1_true': f1truelist, 'grad_true': gradtruelist}
     data = {'fbest': fbestlist,'sol': sollist, 'freq': freqlist, 'time': timelist, 'nPop': poplist, 'f1': f1list, 'grad': gradlist}
     df = pd.DataFrame(data)
     df.to_csv("data.csv")
